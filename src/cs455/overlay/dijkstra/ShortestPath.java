53caed84f4988e748e3b3e1865f1ab8fbce46981
nmalensek <nicholas@Nicholass-MacBook-Air.local>
2017-02-05 18:41:57 -0700

    added code for dijkstra

--- src/cs455/overlay/dijkstra/ShortestPath.java
+++ src/cs455/overlay/dijkstra/ShortestPath.java
@@ -1,4 +1,107 @@
 package cs455.overlay.dijkstra;
 
+import java.util.*;
+
 public class ShortestPath {
+    private final List<Vertex> nodes;
+    private final List<Edge> edges;
+    private Set<Vertex> settledNodes;
+    private Set<Vertex> unSettledNodes;
+    private Map<Vertex, Vertex> predecessors;
+    private Map<Vertex, Integer> distance;
+
+    public ShortestPath(Graph graph) {
+        this.nodes = new ArrayList<>(graph.getVertices());
+        this.edges = new ArrayList<>(graph.getEdges());
+    }
+
+    public void execute(Vertex source) {
+        settledNodes = new HashSet<>();
+        unSettledNodes = new HashSet<>();
+        distance = new HashMap<>();
+        predecessors = new HashMap<>();
+        distance.put(source, 0);
+        unSettledNodes.add(source);
+        while (unSettledNodes.size() > 0) {
+            Vertex node = getMinimum(unSettledNodes);
+            settledNodes.add(node);
+            unSettledNodes.remove(node);
+            findMinimalDistances(node);
+        }
+    }
+
+    private void findMinimalDistances(Vertex node) {
+        List<Vertex> adjacentNodes = getNeighbors(node);
+        for (Vertex target : adjacentNodes) {
+            if(getShortestDistance(target) > getShortestDistance(node) + getDistance(node, target)) {
+                distance.put(target, getShortestDistance(node) + getDistance(node, target));
+                predecessors.put(target, node);
+                unSettledNodes.add(target);
+            }
+        }
+    }
+
+    private int getDistance(Vertex node, Vertex target) {
+        for(Edge edge : edges) {
+            if(edge.getSource().equals(node) && edge.getDestination().equals(target)) {
+                return edge.getWeight();
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    private List<Vertex> getNeighbors(Vertex node) {
+        List<Vertex> neighbors = new ArrayList<>();
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(node) && !isSettled(edge.getDestination())) {
+                neighbors.add(edge.getDestination());
+            }
+        }
+        return neighbors;
+    }
+
+    private Vertex getMinimum(Set<Vertex> vertices) {
+        Vertex minimum = null;
+        for (Vertex vertex : vertices) {
+            if (minimum == null) {
+                minimum = vertex;
+            } else if (getShortestDistance(vertex) < getShortestDistance(minimum)){
+                minimum = vertex;
+            }
+        }
+        return minimum;
+    }
+
+    private boolean isSettled(Vertex vertex) {
+        return settledNodes.contains(vertex);
+    }
+
+    private int getShortestDistance(Vertex destination) {
+        Integer d = distance.get(destination);
+        if(d == null) {
+            return Integer.MAX_VALUE;
+        } else {
+            return d;
+        }
+    }
+
+    public LinkedList<Vertex> getPath(Vertex target) {
+        LinkedList<Vertex> path = new LinkedList<>();
+        Vertex step = target;
+        // check if a path exists
+        if (predecessors.get(step) == null) {
+            return null;
+        }
+        path.add(step);
+        while (predecessors.get(step) != null) {
+            step = predecessors.get(step);
+            path.add(step);
+        }
+        // Put it into the correct order
+        Collections.reverse(path);
+        return path;
+    }
+
 }
+
+
